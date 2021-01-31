2f0bffa00b6b428e5550efce9e2e3397ab7170fd
Rogerio Fonseca Goncalves <rogerio.fonseca@gmail.com>
2021-01-31 06:13:11 -0300

    :white_check_mark: (tests): implementing tests for generic search

---
+++ CHANGELOG.md
@@ -0,0 +1,1 @@
+- implementing tests for generic search
--- tests/main.spec.js
+++ tests/main.spec.js
@@ -1,4 +1,10 @@
-import { expect } from 'chai';
+import chai, { expect } from 'chai';
+import sinon from 'sinon';
+import sinonChai from 'sinon-chai';
+
+chai.use(sinonChai);
+global.fetch = require('node-fetch');
+
 import { search, searchAlbums, searchArtists, searchTracks, searchPlaylists } from '../src/main';
 
 describe('Spotify API Wrapper', () => {
@@ -23,5 +29,54 @@ describe('Spotify API Wrapper', () => {
     it('should exist the searchPlaylists method', () => {
       expect(searchPlaylists).to.exist;
     });
+  });
+
+  describe('Generic Serach', () =>{
+    let fetchedStub;
+    let promise;
+
+    beforeEach( () => {
+      fetchedStub = sinon.stub(global, 'fetch');
+      promise = fetchedStub.resolves({ json: () => ({ album: 'name' }) });
+    });
+
+    afterEach( () => {
+      fetchedStub.restore();
+    });
+
+    it('should call fetch functionsdsds', () => {
+      const artists = search();
+      expect(fetchedStub).to.have.been.calledOnce;
+    });
+
+    it('should call fetch with the correct URL', () => {
+      context('passing one type', () => {
+        const artists = search('Incubus', 'artist');
+
+        expect(fetchedStub).to.have.been
+        .calledWith('https://api.spotify.com/v1/search?q=Incubus&type=artist');
+
+        const albums = search('Incubus', 'album');
+
+        expect(fetchedStub).to.have.been
+        .calledWith('https://api.spotify.com/v1/search?q=Incubus&type=album');
+      });
+
+      context('passing more than one type', () => {
+        const artistsAndAlbums = search('Incubus', ['artist', 'album']);
+
+        expect(fetchedStub).to.have.been
+          .calledWith('https://api.spotify.com/v1/search?q=Incubus&type=artist,album');
+      });
+    });
+
+    it('should return the JSON Data from the Promise', () => {
+      const artists = search('Incubus', 'artist');
+
+      artists.then(data => {
+        expect(data).to.be.eql({ body: 'json' })
+       })
+    });
+
   });
 });
